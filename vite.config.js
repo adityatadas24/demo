d4f849005d81fdfbd2fd7a28b455b757a95428e9
adityatadas24 <adityatadas2000@gmail.com>
2024-12-05 23:12:14 +0530

    sone

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',  // Ensure the build output goes to the 'dist' folder
+    rollupOptions: {
+      input: 'index.html',  // Make sure Vite knows to use index.html as the entry point
+    }
+  },
 })
